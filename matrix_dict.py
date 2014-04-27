02bd90ec2e09861c12b10cb6c471bea1044da3a6
hanpari <hanpari@seznam.cz>
2014-04-27 19:53:38 +0200

    2

--- matrix_dict.py
+++ matrix_dict.py
@@ -1,4 +1,4 @@
-def list_to_dict(data):
+def list_to_dict(matrix):
     """
     Convert list matrix to dict matrix
 
@@ -7,13 +7,18 @@ def list_to_dict(data):
     http://www.checkio.org/mission/open-labyrinth/publications/Miaou/python-3/dijkstras-forever/#comment-outer-5200
 
     Especially:
-    "BTW, checkio seems to have a lot of problems involving matrixes represented as lists of lists.
-    The notation grid[i][j] looks kind of messy; if you were to use a single dict with tuples (i, j)
-    as keys you could make the code look a little cleaner since you could write grid[i, j] instead.
-    (Although the key tuples would end up being more expensive.)" (Guido van Rossum) 
+    "BTW, checkio seems to have a lot of problems
+    involving matrixes represented as lists of lists.
+    The notation grid[i][j] looks kind of messy;
+    if you were to use a single dict with tuples (i, j)
+    as keys you could make the code look a little cleaner
+    since you could write grid[i, j] instead.
+    (Although the key tuples would end up being more expensive.)"
+    (Guido van Rossum) 
 
     IMPROVED READABILITY?
-    If you feel like this you are encouraged to express your opinion if such step improved readability or not.
+    If you feel like this you are encouraged
+    to express your opinion if such step improved readability or not.
     
     DOCTEST
     >>> matrix = [[1,2,3,4,5],\
@@ -25,8 +30,8 @@ def list_to_dict(data):
     
     result = dict()
     x = 0
-    for li in data:
-        for y, val in enumerate(li):
+    for element in matrix:
+        for y, val in enumerate(element):
             result[x,y] = val
         x += 1
     return result
