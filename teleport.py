eed0bfa1630f0d7285f415e210bfe9b6c47e02cf
hanpari <hanpari@seznam.cz>
2014-05-04 16:13:52 +0200

    8 teleport
    Teleport is not working properly because of two station in dictionairy

--- teleport.py
+++ teleport.py
@@ -1,35 +1,52 @@
 from pprint import pprint
+from random import shuffle
 def checkio(ports):
     gates = dict()
-    path = ["0"] #just to avoid index error in path[-2]
-    ports = ports.split(",")
+    path = ["0"] #just to avoid IndexError in path[-2]
+    routes = ports.split(",")
+    shuffle(routes)
     checks = list("12345678")
     for gate in range(1,9):
         gate = str(gate)
         gates[gate] = [s.replace(gate, "") for
-                       s in ports if
+                       s in routes if
                        gate in s]
+    pprint(gates)
+    return None
     #to demonstrate what is checks for.
     path.append(checks.pop(0)) #append first gate "1"
-    #count = 0 #to prevent endless loop
-    while(True):
-        #count+=1
-        #if count >50: break #sorry, Bryug
+    count=len(routes)+10
+    while(count):
+        count -= 1
         ways_to_go = gates[path[-1]]
-        way = ways_to_go[0]
+        if len(ways_to_go):
+            way = ways_to_go[0] 
+        else:
+            gates.pop(path[-1])
+            return checkio(ports)
         if not checks and way == "1":
             path.append(way) 
             break
         if  way != path[-1] and way != path[-2]:
             path.append(way)
+            ways_to_go.remove(way)
+            #gates[way].remove(path[-1])
             try: 
                 checks.remove(way)
             except ValueError:
-                pass
-        ways_to_go.append(ways_to_go.pop(0)) #append first way as last
-
-    #print("".join(path)[1:])
+                # one mission accomplished
+                count = 10 #10 attempts to find exit
+        else:
+              ways_to_go.append(ways_to_go.pop(0)) #append first way as last
+    else:
+        return checkio(ports)
     return "".join(path)[1:]
+
+
+print(
+    
+    checkio("12,28,87,71,13,14,34,35,45,46,63,65")
+    )
             
             
 
@@ -43,8 +60,8 @@ def checkio(ports):
 
 
 
-checkio("12,28,87,71,13,14,34,35,45,46,63,65")
-checkio("12,23,34,45,56,67,78,81") 
+print(checkio("12,28,87,71,13,14,34,35,45,46,63,65"),
+checkio("12,23,34,45,56,67,78,81")) 
 checkio("12,28,87,71,13,14,34,35,45,46,63,65") 
 checkio("12,15,16,23,24,28,83,85,86,87,71,74,56") 
 checkio("13,14,23,25,34,35,47,56,58,76,68") 
