1898215afe82d901427fe29b84b1816235c42ca9
hanpari <hanpari@seznam.cz>
2014-06-01 15:36:45 +0200

    16 Pyramida

--- pyramid.py
+++ pyramid.py
@@ -1,13 +1,16 @@
 def count_gold(data):
-    last_result = 0
-    for i, val in enumerate(data[-1]):
+    result = list(data[-1]) 
+    for i in range(len(data)-2, -1, -1):
+        for v, value in enumerate(data[i]):
+            result[v] = value + max(result[v], result[v+1])
+    return result[0] 
+    
         
 
 
 
-
-
-count_gold((
+if __name__ == "__main__":
+    print(count_gold((
     (1,),
     (2, 3),
     (3, 3, 1),
@@ -15,4 +18,22 @@ count_gold((
     (3, 1, 3, 1, 3),
     (2, 2, 2, 2, 2, 2),
     (5, 6, 4, 5, 6, 4, 3)
-)) == 23
+)) == 23)
+
+
+    print(count_gold((
+    (1,),
+    (2, 1),
+    (1, 2, 1),
+    (1, 2, 1, 1),
+    (1, 2, 1, 1, 1),
+    (1, 2, 1, 1, 1, 1),
+    (1, 2, 1, 1, 1, 1, 9)
+)) == 15)
+
+    print(count_gold((
+    (9,),
+    (2, 2),
+    (3, 3, 3),
+    (4, 4, 4, 4)
+)) == 18)
