44bc28aea57954e96d783575fda9ab19e4ed5e1f
hanpari <hanpari@seznam.cz>
2014-06-20 21:40:12 +0200

    20 Golf pred optimalizaci

--- palid_prime_golf.py
+++ palid_prime_golf.py
@@ -1,14 +1,14 @@
 
 def golf2(n):
  s,k=set(),2
- while(1):
+ while 1:
   s.update(set(i for i in range(k,n*n,k)));k+=1
   if k not in s and k>n and int(str(k)[::-1])==k:return k if n>1 else 2
   
 def golf(n):
- while(1):
+ while 1:
   n+=1
-  if all(n%i!=0 for i in range(2,n)) and int(str(n)[::-1])==n:return n
+  if all(n%i for i in range(2,n))and int(str(n)[::-1])==n:return n
     
     
 
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     assert golf(2) == 3
     assert golf(13) == 101
     assert golf(101) == 131
-    #assert golf(999999) == 1003001
+    assert golf(999999) == 1003001
--- teleport.py
+++ teleport.py
@@ -1,4 +1,40 @@
-def checkio(ports,
+path = ""
+def checkio(ports, start="1"):
+    ports += ","
+    if start not in ports:
+        return ""
+    
+    for port in ports.split(","):
+        if start in port:
+            path = start + checkio(
+                        ports.replace(port + ",", ""),
+                        port.replace(start, ""))
+            return path                   
+##            if (path[-1] == "1" and
+##                set(path) == set("12345678")):
+##                return path
+    else:
+        return ""
+                
+            
+    
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def checkio2(ports,
             pos = "1",
             checkpoints = "2345678"):
     if "1" not in ports:
@@ -24,7 +60,7 @@ def checkio(ports,
 
 
 
-#print(checkio("12,23,34,45,56,67,78,81") == "123456781")
+print(checkio("12,23,34,45,56,67,78,81"))
 #print(checkio("12,28,87,71,13,14,34,35,45,46,63,65"))# == "1365417821"
 #print(checkio("12,15,16,23,24,28,83,85,86,87,71,74,56"))# == "12382478561"
 #print(checkio("13,14,23,25,34,35,47,56,58,76,68"))# == "132586741"
